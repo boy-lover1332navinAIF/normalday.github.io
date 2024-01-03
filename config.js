8698f8b8e7a42e08c76c5b8ef2c5bbdcda17ff8e
kyoo-147 <baemyungkang2806@gmail.com>
2024-01-03 11:01:51 +0700

    open it

---
+++ config.js
@@ -0,0 +1,51 @@
+var config = {
+    texts: [
+        "Open it, my princess",
+        "Dear baby",
+        "Today is a normal day",
+        "But I miss you",
+        "So I did this",
+        "However, it's because of google translation",
+        "I can imagine your smiling face",
+        "Cute appearance",
+        "The face of judgment",
+        "Everything I like",
+        "By the way, let me tell you!",
+        "This year I hope you eat well",
+        "Feedy",
+        "Then you will have the strength to work",
+        "And miss me!",
+        "Happy normal day~.~Love you wife!",
+    ],
+    imgs: {
+        "Open it, my princess": "./imgs/songxinxin.jpeg",
+        "Dear baby": "./imgs/nothing.jpg",
+        "Today is a normal day": "./imgs/shengri.jpeg",
+        "But I miss you": "./imgs/zaiyiqi.png",
+        "So I did this": "./imgs/nothing.jpg",
+        "However, it's because of google translation": "./imgs/yangzi.jpeg",
+        "I can imagine your smiling face": "./imgs/nothing.jpg",
+        "Cute appearance": "./imgs/nothing.jpg",
+        "The face of judgment": "./imgs/nothing.jpg",
+        "Everything I like": "./imgs/nothing.jpg",
+        "By the way, let me tell you!": "./imgs/chuo.gif",
+        "This year I hope you eat well": "./imgs/chihaodian.jpeg",
+        "Feedy": "./imgs/weibao.jpeg",
+        "Then you have the strength to work": "./imgs/em.jpg",
+        "And miss me!": "./imgs/nothing.png",
+        "Happy normal day~.~Love you wife!": "./imgs/em.jpg"
+    },
+    
+    desc: {
+        turn_on: "Bắt đầu nào!!!",
+        play: "Lên cho trẫm con beat đê!!!",
+        bannar_coming: "Cho anh tí màu đê",
+        balloons_flying: "Có vẻ thiếu cái gì đó rồi bay",
+        cake_fadein: "Bánh đâu?",
+        light_candle: "Nến đâu?",
+        wish_message: "Dâng lên cho vợ ta đyyy",
+        story: "Giang sơn này ta dành cho nàng"
+    },
+
+    loveText: 'Love you, MaiVy của ta >.- U are my Roxana'
+};
